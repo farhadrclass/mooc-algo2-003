33c36c8a6f3b0f3285bd0a2b362da2921cc25acb
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-09-03 15:07:43 -0700

    Added load_data and generate_graph help functions to q3

--- A1/tests/test_q3.py
+++ A1/tests/test_q3.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python
 
 from heap.heap import Heap
+from collections import defaultdict
+import numpy
 import random
 import unittest
+
+def load_data(file_name, skiprows=1):
+    """ Returns matrix of  node and edges from edges.txt"""
+    with open(file_name, 'r') as f:
+        num_nodes, num_edges = [int(n) for n in f.readline().strip().split()]
+
+    node_matrix = numpy.loadtxt(file_name, skiprows=skiprows)
+
+    return num_nodes, num_edges, node_matrix
+
+
+def generate_graph(node_matrix):
+    """Takes as input a matrix where rows are edges and columns are [node1 node2 weight]"""
+    graph = defaultdict(dict)
+
+    for item in node_matrix:
+        node1, node2, weight = [int(n) for n in item]
+        graph[node1][node2] = weight
+        graph[node2][node1] = weight
+
+    return graph
 
 
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
 
-        self.file_name = 'edges.txt'
+        self.file_name = './data/edges.txt'
         self.num_nodes, self.num_edges, self.node_matrix = load_data(
             self.file_name)
         self.graph = generate_graph(self.node_matrix)
@@ -17,6 +40,8 @@ class TestSequenceFunctions(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_q3(self):
+        print self.graph
 
 
 if __name__ == '__main__':
