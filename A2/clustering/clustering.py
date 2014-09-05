fe629b6a87d6261aebf5d9d2fd4d2553e76a66f8
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-09-05 15:49:14 -0700

    Add union, find methods to clustering.py
    Added code for union and find, as well as comments.

--- A2/clustering/clustering.py
+++ A2/clustering/clustering.py
@@ -1,25 +1,46 @@
 """
     clustering.py
     ~~~~~~~~~~~~
-    Implements UnionFind datastructure supporting the following operations:
-        - find in O(1) time
-        - union in O(logE) time, where E is # edges
+
 """
 
 from collections import defaultdict
 
 
 class UnionFind(object):
+
+    """Implements UnionFind datastructure supporting the following operations:
+        - find in O(1) time
+        - union in O(logV) time, where V is # vertices
+    """
+
+    def __init__(self, nodes=[]):
+        """Inits leader and set lookup dictionaries:
+            leader_lookup  - { node : leader node }
+            set_lookup - { leader node : list of nodes in leader node's set }
+        """
+        self.leader_lookup = {node: node for node in nodes}
+        self.set_lookup = {node: [node] for node in nodes}
     pass
 
     def union(leader1, leader2):
-        pass
+        "Merges the sets of leader1 and leader 2, taking the new leader to be the larger of the two"
+        size_leader1 = len(self.set_lookup[leader1])
+        size_leader2 = len(self.set_lookup[leader2])
 
-    def find(node1, node2):
-        pass
+        (new_leader, old_leader) = (leader1, leader2) if size_leader1 >= size_leader2 else (leader2, leader1)
+
+        old_set = self.set_lookup[old_leader]
+        self.set_lookup[new_leader].append(old_set)
+
+        for node in old_set:
+            self.leader_lookup[node] = new_leader
+
+    def find(node):
+        "Return the leader node which 'node' points to."
+        return self.leader_lookup[node]
 
 
 def kruskal(graph):
-    # mst == minimum spanning tree
-    mst = defaultdict(dict)
+    mst = defaultdict(dict)  # minimum spanning tree
     return mst
