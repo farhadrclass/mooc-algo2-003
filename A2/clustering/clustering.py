834292bd344fbbc241ee5a3c04a812ab3c2b1b43
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-09-05 15:08:14 -0700

    Add skeleton for clustering.py
    Added empty definitions for:
    -UnionFind data structure
    -Kruskal’s MST algorithm

---
+++ A2/clustering/clustering.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+
+class UnionFind(object):
+    pass
+
+    def union(leader1, leader2):
+        pass
+
+    def find(node1, node2):
+        pass
+
+
+def kruskal(graph):
+    # mst == minimum spanning tree
+    mst = defaultdict(dict)
+    return mst
