b7a422c31bcf6f2913104131a2082650099390e9
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-09-05 12:57:02 -0700

    Rename test_cluster.py to test_q1.py

---
+++ A2/test_q1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+    test_clustering.py
+    ~~~~~~~~~~~~
+
+"""
+
+import unittest
+
+def load_data(file_name, skiprows=1):
+    """ Returns matrix of  node and edges from .txt file"""
+    with open(file_name, 'r') as f:
+        num_nodes, num_edges = [int(n) for n in f.readline().strip().split()]
+
+    node_matrix = numpy.loadtxt(file_name, skiprows=skiprows)
+
+    return node_matrix
+
+def generate_graph(node_matrix):
+    """Takes as input a matrix where rows are edges and columns are [node1 node2 weight]"""
+    graph = defaultdict(dict)
+
+    for item in node_matrix:
+        node1, node2, weight = [int(n) for n in item]
+        graph[node1][node2] = weight
+        graph[node2][node1] = weight
+
+    return graph
+
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.node_matrix = load_data('./data/clustering1.txt')
+        self.graph = generate_graph(self.node_matrix)
+        pass
